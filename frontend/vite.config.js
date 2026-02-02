f86bb98bd30435a74047a4c673687e28fabe34b0
noman-hossen <nomansfamily16@gmail.com>
2026-02-02 11:47:57 +0600

    remove favicon plugin to fix build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import favicon from 'vite-plugin-favicon'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    {
-      ...favicon({
-        source: fileURLToPath(new URL('src/assets/logo.png', import.meta.url)),
-        appName: 'Cactus-pen',
-      }),
-      apply: 'build'
-    }
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
